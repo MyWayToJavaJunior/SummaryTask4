40209b09e1f481748c0823cc4b297fe7ce474d8f
epam3_norkin_mark <markinnork7@mail.ru>
2015-01-24 23:51:39 +0200

    add entity faculty subjects

---
+++ src/ua/nure/norkin/SummaryTask4/entity/FacultySubjects.java
@@ -0,0 +1,44 @@
+package ua.nure.norkin.SummaryTask4.entity;
+
+public class FacultySubjects extends Entity {
+
+	private static final long serialVersionUID = 1165092452837127706L;
+	private int subjectId;
+	private int facultyId;
+
+	public FacultySubjects(int subjectId, int facultyId) {
+		super();
+		this.subjectId = subjectId;
+		this.facultyId = facultyId;
+	}
+
+	public FacultySubjects(Subject subject, Faculty faculty) {
+		this(subject.getId(), faculty.getId());
+	}
+
+	public FacultySubjects() {
+	}
+
+	public int getSubjectId() {
+		return subjectId;
+	}
+
+	public void setSubjectId(int subjectId) {
+		this.subjectId = subjectId;
+	}
+
+	public int getFacultyId() {
+		return facultyId;
+	}
+
+	public void setFacultyId(int facultyId) {
+		this.facultyId = facultyId;
+	}
+
+	@Override
+	public String toString() {
+		return "FacultySubjects [subjectId=" + subjectId + ", facultyId="
+				+ facultyId + "]";
+	}
+
+}
