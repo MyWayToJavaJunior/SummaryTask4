70a09bf5712ee069ba9595ac0e9012ecdd682c7e
epam3_norkin_mark <markinnork7@mail.ru>
2015-01-24 23:49:38 +0200

    add subject entity

---
+++ src/ua/nure/norkin/SummaryTask4/entity/Subject.java
@@ -0,0 +1,28 @@
+package ua.nure.norkin.SummaryTask4.entity;
+
+public class Subject extends Entity {
+
+	private static final long serialVersionUID = -5388561545513613948L;
+	private String name;
+
+	public Subject(String name) {
+		this.name = name;
+	}
+
+	public Subject() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Subject [name=" + name + "]";
+	}
+
+}
