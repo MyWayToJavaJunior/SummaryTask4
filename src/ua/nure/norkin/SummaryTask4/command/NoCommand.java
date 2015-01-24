249d532135b6306e907f3dd76cdf4b793fe88322
epam3_norkin_mark <markinnork7@mail.ru>
2015-01-25 00:19:17 +0200

    implement NoCommand which forwards user to error page in cases when no appropriate Command will be found

---
+++ src/ua/nure/norkin/SummaryTask4/command/NoCommand.java
@@ -0,0 +1,38 @@
+package ua.nure.norkin.SummaryTask4.command;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import ua.nure.norkin.SummaryTask4.Path;
+
+/**
+ * No command.
+ *
+ * @author D.Kolesnikov
+ *
+ */
+public class NoCommand extends Command {
+
+	private static final long serialVersionUID = -2785976616686657267L;
+
+	private static final Logger LOG = Logger.getLogger(NoCommand.class);
+
+	@Override
+	public String execute(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+		LOG.debug("Command execution starts");
+
+		String errorMessage = "No such command";
+		request.setAttribute("errorMessage", errorMessage);
+		LOG.error("Set the request attribute: 'errorMessage' = " + errorMessage);
+
+		LOG.debug("Command execution finished");
+		return Path.ERROR_PAGE;
+	}
+
+}
