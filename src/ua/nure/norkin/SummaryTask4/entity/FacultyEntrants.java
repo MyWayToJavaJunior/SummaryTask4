eb3e80a649fa7f7f82393574d05995fc3ab3ee74
epam3_norkin_mark <markinnork7@mail.ru>
2015-01-24 23:51:08 +0200

    add entity faculty entrants

---
+++ src/ua/nure/norkin/SummaryTask4/entity/FacultyEntrants.java
@@ -0,0 +1,44 @@
+package ua.nure.norkin.SummaryTask4.entity;
+
+public class FacultyEntrants extends Entity {
+
+	private static final long serialVersionUID = 1099698953477481899L;
+
+	private int facultyId;
+	private int entrantId;
+
+	public FacultyEntrants(int facultyId, int entrantId) {
+		this.facultyId = facultyId;
+		this.entrantId = entrantId;
+	}
+
+	public FacultyEntrants(Faculty faculty, Entrant entrant) {
+		this(faculty.getId(), entrant.getId());
+	}
+
+	public FacultyEntrants() {
+	}
+
+	public int getFacultyId() {
+		return facultyId;
+	}
+
+	public void setFacultyId(int facultyId) {
+		this.facultyId = facultyId;
+	}
+
+	public int getEntrantId() {
+		return entrantId;
+	}
+
+	public void setEntrantId(int entrantId) {
+		this.entrantId = entrantId;
+	}
+
+	@Override
+	public String toString() {
+		return "FacultyEntrants [facultyId=" + facultyId + ", entrantId="
+				+ entrantId + "]";
+	}
+
+}
