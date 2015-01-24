24b4b84f7b279ff6c772b41f35926c3b19de4f0f
epam3_norkin_mark <markinnork7@mail.ru>
2015-01-24 23:50:06 +0200

    add mark entity

---
+++ src/ua/nure/norkin/SummaryTask4/entity/Mark.java
@@ -0,0 +1,60 @@
+package ua.nure.norkin.SummaryTask4.entity;
+
+public class Mark extends Entity {
+
+	private static final long serialVersionUID = -6225323023971292703L;
+	private int subjectId;
+	private int entrantId;
+	private byte mark;
+	private String examType;
+
+	public Mark(int subjectId, int entrantId, byte mark, String examType) {
+		super();
+		this.subjectId = subjectId;
+		this.entrantId = entrantId;
+		this.mark = mark;
+		this.examType = examType;
+	}
+
+	public Mark() {
+	}
+
+	public int getSubjectId() {
+		return subjectId;
+	}
+
+	public void setSubjectId(int subjectId) {
+		this.subjectId = subjectId;
+	}
+
+	public int getEntrantId() {
+		return entrantId;
+	}
+
+	public void setEntrantId(int entrantId) {
+		this.entrantId = entrantId;
+	}
+
+	public byte getMark() {
+		return mark;
+	}
+
+	public void setMark(byte mark) {
+		this.mark = mark;
+	}
+
+	public String getExamType() {
+		return examType;
+	}
+
+	public void setExamType(String markType) {
+		this.examType = markType;
+	}
+
+	@Override
+	public String toString() {
+		return "Mark [subjectId=" + subjectId + ", entrantId=" + entrantId
+				+ ", mark=" + mark + ", examType=" + examType + "]";
+	}
+
+}
