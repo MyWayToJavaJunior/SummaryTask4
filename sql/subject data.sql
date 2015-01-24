5ff7532c16246754852b4823e0cba172a3b82a02
epam3_norkin_mark <markinnork7@mail.ru>
2015-01-25 00:04:42 +0200

    data for subject table

---
+++ sql/subject data.sql
@@ -0,0 +1,8 @@
+INSERT INTO `subject` (`name`) VALUES ("Математика");
+INSERT INTO `subject` (`name`) VALUES ("Литература");
+INSERT INTO `subject` (`name`) VALUES ("Биология");
+INSERT INTO `subject` (`name`) VALUES ("Химия");
+INSERT INTO `subject` (`name`) VALUES ("Украинский язык");
+INSERT INTO `subject` (`name`) VALUES ("История");
+INSERT INTO `subject` (`name`) VALUES ("Физика");
+INSERT INTO `subject` (`name`) VALUES ("Английский язык");
