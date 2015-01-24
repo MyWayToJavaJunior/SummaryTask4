a15010fd06b1960200b0826a7b5fa792dda1df12
epam3_norkin_mark <markinnork7@mail.ru>
2015-01-24 23:47:15 +0200

    add user entity

---
+++ src/ua/nure/norkin/SummaryTask4/entity/User.java
@@ -0,0 +1,84 @@
+package ua.nure.norkin.SummaryTask4.entity;
+
+/**
+ * User entity.
+ *
+ * @author D.Kolesnikov
+ *
+ */
+public class User extends Entity {
+
+	private static final long serialVersionUID = -6889036256149495388L;
+
+	private String email;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String role;
+
+	public User() {
+	}
+
+	public User(String email, String password, String firstName,
+			String lastName, Role role) {
+		this(email, password, firstName, lastName, role.getName());
+	}
+
+	public User(String email, String password, String firstName,
+			String lastName, String role) {
+		super();
+		this.email = email;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.role = role;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String login) {
+		this.email = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "User [email=" + email + ", password=" + password
+				+ ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", role=" + role + "]";
+	}
+
+}
