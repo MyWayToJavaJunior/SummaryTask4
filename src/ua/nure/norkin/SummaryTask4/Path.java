fb63523cff8d762bdd53e7dd43da0aad7788a234
epam3_norkin_mark <markinnork7@mail.ru>
2015-01-25 21:57:53 +0200

    create handler for path's to the View

---
+++ src/ua/nure/norkin/SummaryTask4/Path.java
@@ -0,0 +1,77 @@
+package ua.nure.norkin.SummaryTask4;
+
+/**
+ * Path holder to jsp pages.
+ *
+ * @author Mark Norkin
+ *
+ */
+public final class Path {
+	// pages
+		public static final String WELCOME_PAGE = "welcome.jsp";
+		public static final String ERROR_PAGE = "/WEB-INF/view/errorPage.jsp";
+
+		public static final String CLIENT_REGISTRATION_PAGE = "registration.jsp";
+		public static final String ADMIN_REGISTRATION_PAGE = "/WEB-INF/view/admin/addAdmin.jsp";
+
+		public static final String ADMIN_PROFILE = "/WEB-INF/view/admin/profile/view.jsp";
+		public static final String ADMIN_PROFILE_EDIT = "/WEB-INF/view/admin/profile/edit.jsp";
+
+		public static final String CLIENT_PROFILE = "/WEB-INF/view/client/profile/view.jsp";
+		public static final String CLIENT_PROFILE_EDIT = "/WEB-INF/view/client/profile/edit.jsp";
+
+		public static final String FACULTY_VIEW_ALL_ADMIN = "/WEB-INF/view/admin/faculty/list.jsp";
+		public static final String FACULTY_VIEW_ALL_CLIENT = "/WEB-INF/view/client/faculty/list.jsp";
+
+		public static final String FACULTY_VIEW_ADMIN = "/WEB-INF/view/admin/faculty/view.jsp";
+		public static final String FACULTY_EDIT_ADMIN = "/WEB-INF/view/admin/faculty/edit.jsp";
+		public static final String FACULTY_ADD_ADMIN = "/WEB-INF/view/admin/faculty/add.jsp";
+
+		public static final String FACULTY_VIEW_CLIENT = "/WEB-INF/view/client/faculty/view.jsp";
+		public static final String FACULTY_APPLY_CLIENT = "/WEB-INF/view/client/faculty/apply.jsp";
+
+		public static final String SUBJECT_VIEW_ALL_ADMIN = "/WEB-INF/view/admin/subject/list.jsp";
+		public static final String SUBJECT_VIEW_ADMIN = "/WEB-INF/view/admin/subject/view.jsp";
+		public static final String SUBJECT_ADD_ADMIN = "/WEB-INF/view/admin/subject/add.jsp";
+		public static final String SUBJECT_EDIT_ADMIN = "/WEB-INF/view/admin/subject/edit.jsp";
+
+		// commands
+		public static final String COMMAND_LIST_ORDERS = "/controller?command=listOrders";
+		public static final String COMMAND_LIST_MENU = "/controller?command=listMenu";
+
+/*
+	// pages
+	public static final String WELCOME_PAGE = "welcome.jsp";
+	public static final String ERROR_PAGE = "/WEB-INF/view/errorPage.jsp";
+
+	public static final String CLIENT_REGISTRATION_PAGE = "registration.jsp";
+	public static final String ADMIN_REGISTRATION_PAGE = "/WEB-INF/view/admin/addAdmin.jsp";
+
+	public static final String ADMIN_PROFILE = "/WEB-INF/view/admin/profile/view.jsp";
+	public static final String ADMIN_PROFILE_EDIT = "/WEB-INF/view/admin/profile/edit.jsp";
+
+	public static final String CLIENT_PROFILE = "/WEB-INF/view/client/profile/view.jsp";
+	public static final String CLIENT_PROFILE_EDIT = "/WEB-INF/view/client/profile/edit.jsp";
+
+	public static final String FACULTY_VIEW_ALL_ADMIN = "/WEB-INF/view/admin/faculty/list.jsp";
+	public static final String FACULTY_VIEW_ALL_CLIENT = "/WEB-INF/view/client/faculty/list.jsp";
+
+	public static final String FACULTY_VIEW_ADMIN = "/WEB-INF/view/admin/faculty/view.jsp";
+	public static final String FACULTY_EDIT_ADMIN = "/WEB-INF/view/admin/faculty/edit.jsp";
+	public static final String FACULTY_ADD_ADMIN = "/WEB-INF/view/admin/faculty/add.jsp";
+
+	public static final String FACULTY_VIEW_CLIENT = "/WEB-INF/view/client/faculty/view.jsp";
+	public static final String FACULTY_APPLY_CLIENT = "/WEB-INF/view/client/faculty/apply.jsp";
+
+	public static final String FACULTY_ENTRANTS_LIST_ADMIN = "/WEB-INF/view/admin/faculty/entrantsList.jsp";
+
+	public static final String SUBJECT_VIEW_ALL_ADMIN = "/WEB-INF/view/admin/subject/list.jsp";
+	public static final String SUBJECT_VIEW_ADMIN = "/WEB-INF/view/admin/subject/view.jsp";
+	public static final String SUBJECT_ADD_ADMIN = "/WEB-INF/view/admin/subject/add.jsp";
+	public static final String SUBJECT_EDIT_ADMIN = "/WEB-INF/view/admin/subject/edit.jsp";
+
+	// commands
+	public static final String COMMAND_LIST_ORDERS = "/controller?command=listOrders";
+	public static final String COMMAND_LIST_MENU = "/controller?command=listMenu";*/
+
+}
