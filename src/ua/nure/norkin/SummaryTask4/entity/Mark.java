201d64efbc894a2672bec1c73d8c88de1c219330
Аня <@127.0.0.1>
2015-01-29 23:52:09 +0200

    add javadoc to mark entity

--- src/ua/nure/norkin/SummaryTask4/entity/Mark.java
+++ src/ua/nure/norkin/SummaryTask4/entity/Mark.java
@@ -1,5 +1,13 @@
 package ua.nure.norkin.SummaryTask4.entity;
 
+/**
+ * Mark entity. Every instance is characterized by foreign keys from subject and
+ * entrant, mark value and exam type (preliminary or diploma). So every mark
+ * references to specific subject and entrant that get it on some exam.
+ *
+ * @author Mark Norkin
+ *
+ */
 public class Mark extends Entity {
 
 	private static final long serialVersionUID = -6225323023971292703L;
