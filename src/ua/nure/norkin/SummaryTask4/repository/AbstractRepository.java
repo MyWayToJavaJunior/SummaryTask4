28b91a8a26d11b609e553915c157eb53c52e2915
Аня <@127.0.0.1>
2015-01-30 01:02:25 +0200

    add javadoc to AbstractRepository

--- src/ua/nure/norkin/SummaryTask4/repository/AbstractRepository.java
+++ src/ua/nure/norkin/SummaryTask4/repository/AbstractRepository.java
@@ -12,13 +12,25 @@ import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
 
+/**
+ * Main parent for all DAO objects that working with relational databases. Declares and implements methods for closing
+ * Connections, ResultSets and Statements. Also performs rollbacking of
+ * transaction.
+ *
+ * @author Mark Norkin
+ *
+ * @param <T> type of entity
+ */
 public abstract class AbstractRepository<T> implements Repository<T> {
 
 	private final static Logger LOG = Logger
 			.getLogger(AbstractRepository.class);
-	protected final DataSource ds /*= init()*/;
+	protected final DataSource ds;
 
-	private DataSource init() {
+	/**
+	 * Initializes DataSource object.
+	 */
+	public AbstractRepository() {
 		DataSource dataSource = null;
 		try {
 			Context initContext = new InitialContext();
@@ -27,17 +39,23 @@ public abstract class AbstractRepository<T> implements Repository<T> {
 		} catch (NamingException ex) {
 			LOG.error("Cannot obtain a connection from the pool", ex);
 		}
-		return dataSource;
+		ds = dataSource;
 	}
 
-	public AbstractRepository() {
-		ds = init();
-	}
-
+	/**
+	 * @return Connection object from the pool.
+	 * @throws SQLException
+	 */
 	protected Connection getConnection() throws SQLException {
 		return ds.getConnection();
 	}
 
+	/**
+	 * Closes given Connection object.
+	 *
+	 * @param con
+	 *            - connection to be closed
+	 */
 	protected void close(Connection con) {
 		if (con != null) {
 			try {
@@ -48,6 +66,12 @@ public abstract class AbstractRepository<T> implements Repository<T> {
 		}
 	}
 
+	/**
+	 * Closes given ResultSet object.
+	 *
+	 * @param rs
+	 *            - ResultSet to be closed.
+	 */
 	protected void close(ResultSet rs) {
 		if (rs != null) {
 			try {
@@ -58,6 +82,12 @@ public abstract class AbstractRepository<T> implements Repository<T> {
 		}
 	}
 
+	/**
+	 * Closes given Statrmrnt object
+	 *
+	 * @param stmt
+	 *            - Statement to be closed
+	 */
 	protected void close(Statement stmt) {
 		if (stmt != null) {
 			try {
