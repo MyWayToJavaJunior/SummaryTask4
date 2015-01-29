eb2709251212a565b15810115fcb561c24390729
Аня <@127.0.0.1>
2015-01-30 00:44:26 +0200

    add javadoc to Repository (DAO) interface

--- src/ua/nure/norkin/SummaryTask4/repository/Repository.java
+++ src/ua/nure/norkin/SummaryTask4/repository/Repository.java
@@ -2,15 +2,57 @@ package ua.nure.norkin.SummaryTask4.repository;
 
 import java.util.Collection;
 
+/**
+ * Main interface for all DAO objects with operations of creating, updating,
+ * deleting and finding some specific object. Also all subclasses should
+ * implement findAll() method, which finds all saved objects of type
+ * <code>T</code> in the database, xml files etc.
+ *
+ * @author Mark Norkin
+ *
+ * @param <T>
+ *            type of the object
+ */
 public interface Repository<T> {
 
+	/**
+	 * Saves specified object in repository.
+	 *
+	 * @param entity
+	 *            - object to be saved
+	 */
 	public void create(T entity);
 
+	/**
+	 * Updates specified object in repository
+	 *
+	 * @param entity
+	 *            - object to be updated
+	 */
 	public void update(T entity);
 
+	/**
+	 * Deletes specified object from repository
+	 *
+	 * @param entity
+	 *            - object to be deleted
+	 */
 	public void delete(T entity);
 
+	/**
+	 * Finds object in repository by it's unique primary key.
+	 *
+	 * @param entityPK
+	 *            - primary key of the object that should be found.
+	 * @return object of type <code>T</code>, which unique id is equal to
+	 *         specified.
+	 */
 	public T find(int entityPK);
 
+	/**
+	 * Finds all objects of type <code>T</code> in repository.
+	 *
+	 * @return all objects that repository has at this point of time.
+	 */
 	public Collection<T> findAll();
 }
