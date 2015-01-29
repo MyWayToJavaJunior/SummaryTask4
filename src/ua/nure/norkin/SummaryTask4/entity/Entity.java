28f3b18682ce57cfb829d871d3757f4972b34eae
Аня <@127.0.0.1>
2015-01-29 23:20:52 +0200

    add javadoc to Entity abstract class

--- src/ua/nure/norkin/SummaryTask4/entity/Entity.java
+++ src/ua/nure/norkin/SummaryTask4/entity/Entity.java
@@ -2,6 +2,13 @@ package ua.nure.norkin.SummaryTask4.entity;
 
 import java.io.Serializable;
 
+/**
+ * Basic common parent for all entities. Provides id field and get/set methods
+ * to him.
+ *
+ * @author Mark Norkin
+ *
+ */
 public abstract class Entity implements Serializable {
 
 	private static final long serialVersionUID = 1113613887250453747L;
