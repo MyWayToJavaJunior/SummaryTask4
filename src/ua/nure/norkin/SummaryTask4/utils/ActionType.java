b5dd5cc90992ef1274bb803573099803b6e2bd6f
Аня <@127.0.0.1>
2015-01-30 00:20:30 +0200

    add javadoc to enum ActionType

--- src/ua/nure/norkin/SummaryTask4/utils/ActionType.java
+++ src/ua/nure/norkin/SummaryTask4/utils/ActionType.java
@@ -1,5 +1,12 @@
 package ua.nure.norkin.SummaryTask4.utils;
 
+/**
+ * Constants, which main purpose is to tell whether user want's to read or write
+ * some data.
+ *
+ * @author Mark Norkin
+ *
+ */
 public enum ActionType {
 	FORWARD, REDIRECT;
 }
