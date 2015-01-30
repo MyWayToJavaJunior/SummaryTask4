016fd5db03834075daeaec8608cf7c97050120bf
Аня <@127.0.0.1>
2015-01-30 22:23:48 +0200

    document administrtor registration command

--- src/ua/nure/norkin/SummaryTask4/command/AdminRegistrationCommand.java
+++ src/ua/nure/norkin/SummaryTask4/command/AdminRegistrationCommand.java
@@ -17,12 +17,26 @@ import ua.nure.norkin.SummaryTask4.repository.UserRepository;
 import ua.nure.norkin.SummaryTask4.utils.ActionType;
 import ua.nure.norkin.SummaryTask4.utils.FieldValidation;
 
+/**
+ * Invoked when administrator wants to add another admin user.
+ *
+ * @author Mark Norkin
+ *
+ */
 public class AdminRegistrationCommand extends Command {
 
 	private static final long serialVersionUID = 1L;
 	private static final Logger LOG = Logger
 			.getLogger(AdminRegistrationCommand.class);
 
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * ua.nure.norkin.SummaryTask4.command.Command#execute(javax.servlet.http
+	 * .HttpServletRequest, javax.servlet.http.HttpServletResponse,
+	 * ua.nure.norkin.SummaryTask4.utils.ActionType)
+	 */
 	@Override
 	public String execute(HttpServletRequest request,
 			HttpServletResponse response, ActionType actionType)
@@ -49,11 +63,22 @@ public class AdminRegistrationCommand extends Command {
 		return result;
 	}
 
+	/**
+	 * Forwards user to registration admin page.
+	 *
+	 * @return path where lie this page
+	 */
 	private String doGet(HttpServletRequest request,
 			HttpServletResponse response) {
 		return Path.FORWARD_ADMIN_REGISTRATION_PAGE;
 	}
 
+	/**
+	 * If validation is successful then admin record will be added in database.
+	 *
+	 * @return after registartion will be completed returns path to welcome
+	 *         page, if not then doGet method will be called.
+	 */
 	private String doPost(HttpServletRequest request,
 			HttpServletResponse response) {
 		String email = request.getParameter(Fields.USER_EMAIL);
