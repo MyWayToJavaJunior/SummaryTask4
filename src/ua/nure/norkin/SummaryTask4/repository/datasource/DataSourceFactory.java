69d610b5026fd295824c51153b69ce49d0be0312
epam3_norkin_mark <markinnork7@mail.ru>
2015-02-08 20:47:36 +0200

    add switch case for new datasource type

--- src/ua/nure/norkin/SummaryTask4/repository/datasource/DataSourceFactory.java
+++ src/ua/nure/norkin/SummaryTask4/repository/datasource/DataSourceFactory.java
@@ -7,6 +7,8 @@ import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlConnectionPoolDataSource;
+
 public abstract class DataSourceFactory {
 
 	private final static Logger LOG = Logger.getLogger(DataSourceFactory.class);
@@ -25,6 +27,19 @@ public abstract class DataSourceFactory {
 			} catch (NamingException e) {
 				LOG.error("Cannot get JNDI DataSource", e);
 			}
+		case MY_SQL_DATASOURCE_WITH_OUT_JNDI:
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				MysqlConnectionPoolDataSource dataSource = new MysqlConnectionPoolDataSource();
+				dataSource
+						.setURL("jdbc:mysql://127.0.0.1:3306/university_admission?useEncoding=true&amp;characterEncoding=UTF-8");
+				dataSource.setUser("root");
+				dataSource.setPassword("password");
+				return dataSource;
+			} catch (ClassNotFoundException e) {
+				LOG.error("Cannot get DataSource without JNDI", e);
+			}
+
 		default:
 			throw new UnsupportedOperationException("No such DataSource: "
 					+ type);
