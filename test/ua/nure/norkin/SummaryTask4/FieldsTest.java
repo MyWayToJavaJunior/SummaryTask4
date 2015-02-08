fd93446f179973ecc3a77a954817adb9c946738d
epam3_norkin_mark <markinnork7@mail.ru>
2015-02-08 23:16:27 +0200

    create dumb tests for path and fields holders

---
+++ test/ua/nure/norkin/SummaryTask4/FieldsTest.java
@@ -0,0 +1,12 @@
+package ua.nure.norkin.SummaryTask4;
+
+import org.junit.Test;
+
+public class FieldsTest {
+
+	@Test
+	public void test() {
+		new Fields();
+	}
+
+}
---
+++ test/ua/nure/norkin/SummaryTask4/PathTest.java
@@ -0,0 +1,12 @@
+package ua.nure.norkin.SummaryTask4;
+
+import org.junit.Test;
+
+public class PathTest {
+
+	@Test
+	public void test() {
+		new Path();
+	}
+
+}
